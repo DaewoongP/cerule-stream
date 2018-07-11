fdf2eb978212cf49cc637f6b7d78e4725250cd16
safecorners <safeconers.mobile@gmail.com>
2018-07-11 11:45:10 +0900

    add Button & test publishing

--- example/ButtonExample/app/src/main/java/io/safecorners/buttonexample/MainActivity.java
+++ example/ButtonExample/app/src/main/java/io/safecorners/buttonexample/MainActivity.java
@@ -2,6 +2,8 @@ package io.safecorners.buttonexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
@@ -15,7 +17,16 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.io.UnsupportedEncodingException;
+
 public class MainActivity extends AppCompatActivity {
+
+    MqttAndroidClient client;
+
+    private Button mUpButton;
+    private Button mDownButton;
+    private Button mLeftButton;
+    private Button mRightButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Create Mqtt Client
         String clientId = MqttClient.generateClientId();
-        MqttAndroidClient client =
-                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+        client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://safecorners.io:1883",
                         clientId);
 
         try {
+            // Connect to a broker
             IMqttToken token = client.connect();
             token.setActionCallback(new IMqttActionListener() {
                 @Override
@@ -47,5 +59,79 @@ public class MainActivity extends AppCompatActivity {
         } catch (MqttException e) {
             e.printStackTrace();
         }
+
+        // Create Button & Binding
+        mUpButton = (Button) findViewById(R.id.up_button);
+        mUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String topic = "ptz/y";
+                String payload = "-3";
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(topic, message);
+                } catch (UnsupportedEncodingException | MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        mDownButton = (Button) findViewById(R.id.down_button);
+        mDownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String topic = "ptz/y";
+                String payload = "3";
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(topic, message);
+                } catch (UnsupportedEncodingException | MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        mLeftButton = (Button) findViewById(R.id.left_button);
+        mLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String topic = "ptz/x";
+                String payload = "-3";
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(topic, message);
+                } catch (UnsupportedEncodingException | MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        mRightButton = (Button) findViewById(R.id.right_button);
+        mRightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String topic = "ptz/x";
+                String payload = "3";
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(topic, message);
+                } catch (UnsupportedEncodingException | MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
