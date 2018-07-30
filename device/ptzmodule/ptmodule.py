ff9d800c2a5c3f1950c9c46e19ea3c86ab8976ed
safecorners <safeconers.mobile@gmail.com>
2018-07-30 15:36:14 +0900

    #38 add nginx:docker-compose.yml file

--- device/ptzmodule/ptmodule.py
+++ device/ptzmodule/ptmodule.py
@@ -1,110 +1,77 @@
-import json
 import paho.mqtt.client as paho
-import ptzmodule.ptz.ptz
+import ptz.ptz
  
-move = ptzmodule.ptz.ptz.move()
+move = ptz.ptz.move()
 
-class bbox:
-    def size(self, userdata, msg):
-        box= str(msg.payload.decode("utf-8", "ignore"))
-        #print(type(box)) # mqtt sub type
-        box = json.loads(box)
-        #print(type(box)) # json load type
+class cam:
+    def size(self, Tw, Th):#bounding box's size return
+        size = Tw * Th
+        print(size)
+        return size
 
-        x1 = box[0]
-        y1 = box[1]
-        x2 = box[2]
-        y2 = box[3]
-        object_name = box[4]
-        
-        w = x2 - x1
-        h = y2 - y1
-        size = w * h
-        print("box name :",object_name)
-        print("size :",size)
+    def delta(self, Cx, Cy, lx, ly):#camera
+        dx = Cx - lx
+        dy = Cy - ly
 
-    def delta(self, userdata, msg):#camera
-        lx = 320
-        ly = 240
-        
-        box= str(msg.payload.decode("utf-8", "ignore"))
-        box = json.loads(box)
-
-        x1 = box[0]
-        y1 = box[1]
-        x2 = box[2]
-        y2 = box[3]
-        
-        cx = (x1 + x2) / 2
-        cy = (y1 + y2) / 2
-
-        dx = cx - lx
-        dy = cy - ly
-
-        if (cx >= 640):
-            cx = 320
-        elif (cx <= 0):
-            cx = 320
-        elif (cy >= 480):
-            cy = 240
-        elif (cy <= 0):
-            cy = 240
-        else:
-            print("centerX :",cx)
-            print("centerY :",cy)
-
-        
         #center vs camera point
-        if (cx != lx):
-            if (dx >= 0):
-                move.x(dx)
-            if (dx <= 0):
-                move.x(dx)
-
-        if (cy != ly):
-            if (dy >= 0):
-                move.y(dy)
-            if (dy <= 0):
-                move.y(dy)
-
-    client = paho.Client()#...
-
-    client.message_callback_add("bbox/size", size)#callback to function
-    client.message_callback_add("bbox/delta", delta)
-
-    client.connect("localhost", 1883)#broker set
-    client.subscribe("bbox/#")#sub 'ptz'
-
-    client.loop_forever()
+        if (Cx != lx):
+            (dx-320)/64+12
+                
+        if (Cy != ly):
+            tilt.ChangeDutyCycle((dy-240)/48+12)
 
 
 class app:
     def on_moveX(object, userdata, msg):
-        t = len(msg.payload)
-        a = int(str(msg.payload)[2:2 + t])
+        a = int(str(msg.payload)[2:4])
         print(a)
+        
         if (a >= 0): #App's key >
             move.right(a)
             
         if (a <= 0): #App's key <
-            move.left(a)
+            move.left(10)
         
     def on_moveY(object, userdata, msg):
-        t = len(msg.payload)
-        a = int(str(msg.payload)[2:2 + t])
+        a = int(str(msg.payload)[2:4])
         print(a)
         
         if (a >= 0): #App's key ^
             move.up(a)
             
         if (a <= 0): #App's key v
-            move.down(a)
+            move.down(10)
+        
+    def on_message(client, userdata, msg):
+        print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+        #size = int(str(msg.payload)[2:3])
 
+<<<<<<< Updated upstream
+    def on_size(object, userdata, msg):
+        a = int(str(msg.payload)[2:4])
+        print(a)
+
+    def on_delta(object, userdata, msg):
+        a = int(str(msg.payload)[2:4])
+        print(a)
     client = paho.Client()#...
+
+    client.message_callback_add("mqtt/size", on_size)#callback to function
+    client.message_callback_add("mqtt/delta", on_delta)
+    client.message_callback_add("mqtt/message", on_message)
+    client.message_callback_add("mqtt/moveX", on_moveX)
+    client.message_callback_add("mqtt/moveY", on_moveY)
+
+    client.connect("localhost", 1883)#broker set
+    client.subscribe("mqtt/#")#sub 'ptz'
+
+    client = paho.Client()  #...
     client.message_callback_add("app/moveX", on_moveX)
     client.message_callback_add("app/moveY", on_moveY)
 
-    client.connect("localhost", 1883)#broker set
-    client.subscribe("app/#")#sub 'ptz'
+    client.connect("localhost", 1883)  #broker set
+    client.subscribe("app/#")  #sub 'ptz'
 
     client.loop_forever()
+
+
